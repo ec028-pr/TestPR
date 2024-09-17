f6f9868001db80f9a2d4be2992b0a2bb691c3539
yisiang <yisiang9728@gmail.com>
2024-09-17 11:42:22 -0500

    last try

--- app/page.js
+++ app/page.js
@@ -3,9 +3,6 @@ export default function Home() {
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
-      <h1>Welcome to this NextJS Course!</h1>
-      <h1>Welcome to this NextJS Course!</h1>
-      <h1>ABCD123</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
     </main>
   );
